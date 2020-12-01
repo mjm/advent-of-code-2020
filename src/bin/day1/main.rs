fd789f101aa39dd86fd76b35d5359e754a9ae042
Matt Moriarity <matt@mattmoriarity.com>
2020-11-30 22:46:39 -0700

    Solve day 1 part 1

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Welcome to Day 1 of Advent of Code!")
+    let args: Vec<String> = env::args().collect();
+
+    let input_path = &args[1];
+    println!("Reading input from {}", input_path);
+
+    let contents = fs::read_to_string(input_path)
+        .expect("Something went wrong reading the file");
+
+    let nums = to_numbers(&contents);
+
+    for (i, num) in nums.iter().enumerate() {
+        for (j, num2) in nums.iter().enumerate() {
+            if i != j && num + num2 == 2020 {
+                println!("The product of {} and {} is {}", num, num2, num * num2);
+                return
+            }
+        }
+    }
+}
+
+fn to_numbers(contents: &str) -> Vec<i32> {
+    contents.lines().map(|s| s.parse::<i32>().unwrap()).collect()
 }
