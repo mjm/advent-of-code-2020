51849cea96a12d0a0a22c0358bafdea21b07b567
Matt Moriarity <matt@mattmoriarity.com>
2020-12-05 23:05:05 -0700

    Solve day 6 part 2

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -13,20 +13,32 @@ fn main() {
     let groups = parse_groups(&contents);
 
     part1(&groups);
-    part2(&groups);
+
+    let groups2 = parse_groups2(&contents);
+    part2(&groups2);
 }
 
 fn part1(groups: &Vec<HashSet<char>>) {
     let result: usize = groups.iter().map(HashSet::len).sum();
-    println!("The sum of counts of answered questions is {}", result);
+    println!("The sum of counts of questions answered by anyone is {}", result);
 }
 
 fn part2(groups: &Vec<HashSet<char>>) {
-
+    let result: usize = groups.iter().map(HashSet::len).sum();
+    println!("The sum of counts of questions answered by everyone is {}", result);
 }
 
 fn parse_groups(contents: &str) -> Vec<HashSet<char>> {
     contents.split("\n\n").map(|group_str| {
         group_str.chars().filter(|c| *c >= 'a' && *c <= 'z').collect::<HashSet<char>>()
+    }).collect()
+}
+
+fn parse_groups2(contents: &str) -> Vec<HashSet<char>> {
+    contents.split("\n\n").map(|group_str| {
+        let people = group_str.lines().collect::<Vec<&str>>();
+        ('a'..='z').filter(|c| {
+            people.iter().all(|person| person.contains(*c))
+        }).collect::<HashSet<char>>()
     }).collect()
 }
