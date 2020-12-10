2182b90fa1c66c3ce23427071c3892f7c93e9c59
Matt Moriarity <matt@mattmoriarity.com>
2020-12-09 22:36:16 -0700

    Solve day 10 part 1

---
+++ src/bin/day10/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::fs;
+use std::num::ParseIntError;
+use nom::lib::std::collections::HashMap;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let input_path = &args[1];
+    println!("Reading input from {}", input_path);
+
+    let contents = fs::read_to_string(input_path)
+        .expect("Something went wrong reading the file");
+    let nums = contents.lines().map(|s| s.parse::<u32>()).collect::<Result<Vec<u32>, ParseIntError>>()
+        .expect("Could not parse lines as numbers");
+
+    part1(&nums);
+    part2(&nums);
+}
+
+fn part1(nums: &Vec<u32>) {
+    let mut nums = nums.clone();
+    nums.sort();
+
+    nums.push(nums[nums.len() - 1] + 3);
+    nums.insert(0, 0);
+
+    let mut counts: HashMap<u32, u32> = HashMap::new();
+    for n in nums.iter().zip(&nums[1..]).map(|(a, b)| *b - *a) {
+        counts.insert(n, counts.get(&n).cloned().unwrap_or(0) + 1);
+    }
+
+    let result = *counts.get(&1).unwrap() * *counts.get(&3).unwrap();
+    println!("The product of the count of 1-diffs and 3-diffs is {}", result);
+}
+
+fn part2(nums: &Vec<u32>) {
+
+}
