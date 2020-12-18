d3cf815612713a439422c9691ae131df72b9e563
Matt Moriarity <matt@mattmoriarity.com>
2020-12-17 22:38:04 -0700

    Solve day 17 part 2

--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -7,10 +7,17 @@ fn main() {
     let contents = read_input();
 
     part1(&contents);
+    part2(&contents);
 }
 
 fn part1(contents: &str) {
-    let mut sim = Simulation::from(contents);
+    let mut sim = Simulation::from_3d(contents);
     sim.simulate(6);
     println!("The number of active points after 6 cycles is {}", sim.num_active_points());
+}
+
+fn part2(contents: &str) {
+    let mut sim = Simulation::from_4d(contents);
+    sim.simulate(6);
+    println!("The number of active points after 6 cycles in 4D is {}", sim.num_active_points());
 }
--- src/bin/day17/sim.rs
+++ src/bin/day17/sim.rs
@@ -1,10 +1,13 @@
 use std::collections::HashSet;
+use std::hash::Hash;
 
-pub trait Point where Self: Sized {
+pub trait Point where Self: Sized + Eq + Hash {
     fn neighbors(&self) -> Vec<Self>;
     fn points_to_check(&self, other: &Self) -> Vec<Self>;
     fn partwise_min(points: &HashSet<Self>) -> Self;
     fn partwise_max(points: &HashSet<Self>) -> Self;
+    fn partwise_decrease(&mut self, other: &Self);
+    fn partwise_increase(&mut self, other: &Self);
 }
 
 #[derive(Debug, Eq, PartialEq, Hash)]
@@ -48,17 +51,120 @@ impl Point for Point3D {
             points.iter().map(|Point3D(_, _, z)| *z).max().unwrap(),
         )
     }
+
+    fn partwise_decrease(&mut self, other: &Self) {
+        if other.0 < self.0 {
+            self.0 = other.0;
+        }
+        if other.1 < self.1 {
+            self.1 = other.1;
+        }
+        if other.2 < self.2 {
+            self.2 = other.2;
+        }
+    }
+
+    fn partwise_increase(&mut self, other: &Self) {
+        if other.0 > self.0 {
+            self.0 = other.0;
+        }
+        if other.1 > self.1 {
+            self.1 = other.1;
+        }
+        if other.2 > self.2 {
+            self.2 = other.2;
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct Point4D(i32, i32, i32, i32);
+
+impl Point for Point4D {
+    fn neighbors(&self) -> Vec<Self> {
+        let &Point4D(x, y, z, w) = self;
+        (-1..=1).flat_map(|dx| {
+            (-1..=1).flat_map(move |dy| {
+                (-1..=1).flat_map(move |dz| {
+                    (-1..=1).map(move |dw| Point4D(x + dx, y + dy, z + dz, w + dw))
+                })
+            })
+        }).filter(|point| point != self).collect()
+    }
+
+    fn points_to_check(&self, other: &Self) -> Vec<Self> {
+        let Point4D(x, y, z, w) = self;
+        let Point4D(x2, y2, z2, w2) = other;
+
+        ((x-1)..=(x2+1)).flat_map(|x| {
+            ((y-1)..=(y2+1)).flat_map(move |y| {
+                ((z-1)..=(z2+1)).flat_map(move |z| {
+                    ((w-1)..=(w2+1)).map(move |w| {
+                        Point4D(x, y, z, w)
+                    })
+                })
+            })
+        }).collect()
+    }
+
+    fn partwise_min(points: &HashSet<Self>) -> Self {
+        Point4D(
+            points.iter().map(|Point4D(x, _, _, _)| *x).min().unwrap(),
+            points.iter().map(|Point4D(_, y, _, _)| *y).min().unwrap(),
+            points.iter().map(|Point4D(_, _, z, _)| *z).min().unwrap(),
+            points.iter().map(|Point4D(_, _, _, w)| *w).min().unwrap(),
+        )
+    }
+
+    fn partwise_max(points: &HashSet<Self>) -> Self {
+        Point4D(
+            points.iter().map(|Point4D(x, _, _, _)| *x).max().unwrap(),
+            points.iter().map(|Point4D(_, y, _, _)| *y).max().unwrap(),
+            points.iter().map(|Point4D(_, _, z, _)| *z).max().unwrap(),
+            points.iter().map(|Point4D(_, _, _, w)| *w).max().unwrap(),
+        )
+    }
+
+    fn partwise_decrease(&mut self, other: &Self) {
+        if other.0 < self.0 {
+            self.0 = other.0;
+        }
+        if other.1 < self.1 {
+            self.1 = other.1;
+        }
+        if other.2 < self.2 {
+            self.2 = other.2;
+        }
+        if other.3 < self.3 {
+            self.3 = other.3;
+        }
+    }
+
+    fn partwise_increase(&mut self, other: &Self) {
+        if other.0 > self.0 {
+            self.0 = other.0;
+        }
+        if other.1 > self.1 {
+            self.1 = other.1;
+        }
+        if other.2 > self.2 {
+            self.2 = other.2;
+        }
+        if other.3 > self.3 {
+            self.3 = other.3;
+        }
+    }
 }
 
 #[derive(Debug)]
-pub struct Simulation {
-    active_points: HashSet<Point3D>,
-    min_corner: Point3D,
-    max_corner: Point3D,
+pub struct Simulation<P: Point> {
+    active_points: HashSet<P>,
+    min_corner: P,
+    max_corner: P,
 }
 
-impl Simulation {
-    pub fn from(s: &str) -> Self {
+impl Simulation<Point3D> {
+    pub fn from_3d(s: &str) -> Self {
         let points: HashSet<Point3D> = s.lines().enumerate().flat_map(|(y, line)| {
             line.chars().enumerate().filter_map(move |(x, c)| {
                 match c {
@@ -77,12 +183,33 @@ impl Simulation {
             max_corner: max,
         }
     }
+}
 
+impl Simulation<Point4D> {
+    pub fn from_4d(s: &str) -> Self {
+        let points: HashSet<Point4D> = s.lines().enumerate().flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                match c {
+                    '#' => Some(Point4D(x as i32, y.clone() as i32, 0, 0)),
+                    _ => None,
+                }
+            })
+        }).collect();
+
+        let min = Point4D::partwise_min(&points);
+        let max = Point4D::partwise_max(&points);
+
+        Simulation {
+            active_points: points,
+            min_corner: min,
+            max_corner: max,
+        }
+    }
+}
+
+impl<P: Point> Simulation<P> {
     pub fn step(&mut self) {
-        let Point3D(mut min_x, mut min_y, mut min_z) = self.min_corner;
-        let Point3D(mut max_x, mut max_y, mut max_z) = self.max_corner;
-
-        let mut changes: Vec<(Point3D, bool)> = Vec::new();
+        let mut changes: Vec<(P, bool)> = Vec::new();
 
         for point in self.min_corner.points_to_check(&self.max_corner) {
             if self.active_points.contains(&point) {
@@ -92,27 +219,9 @@ impl Simulation {
                 }
             } else {
                 if self.num_active_neighbors(&point) == 3 {
-                    let Point3D(x, y, z) = point;
+                    self.min_corner.partwise_decrease(&point);
+                    self.max_corner.partwise_increase(&point);
                     changes.push((point, true));
-
-                    if x < min_x {
-                        min_x = x;
-                    }
-                    if y < min_y {
-                        min_y = y;
-                    }
-                    if z < min_z {
-                        min_z = z;
-                    }
-                    if x > max_x {
-                        max_x = x;
-                    }
-                    if y > max_y {
-                        max_y = y;
-                    }
-                    if z > max_z {
-                        max_z = z;
-                    }
                 }
             }
         }
@@ -124,9 +233,6 @@ impl Simulation {
                 self.active_points.remove(&point);
             }
         }
-
-        self.min_corner = Point3D(min_x, min_y, min_z);
-        self.max_corner = Point3D(max_x, max_y, max_z);
     }
 
     pub fn simulate(&mut self, num_cycles: usize) {
@@ -139,7 +245,7 @@ impl Simulation {
         self.active_points.len()
     }
 
-    fn num_active_neighbors(&self, point: &Point3D) -> usize {
+    fn num_active_neighbors(&self, point: &P) -> usize {
         point.neighbors().iter()
             .filter(|p| self.active_points.contains(*p))
             .count()
