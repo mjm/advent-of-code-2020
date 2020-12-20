d41443ffd0007223c0747140c15c2a6069a1e6d6
Matt Moriarity <matt@mattmoriarity.com>
2020-12-20 12:56:21 -0700

    Solve day 19 part 2

--- src/bin/day19/main.rs
+++ src/bin/day19/main.rs
@@ -6,12 +6,21 @@ mod parse;
 
 fn main() {
     let contents = read_input();
-    let (rules, input_strs) = parse_input(&contents).unwrap();
+    let (mut rules, input_strs) = parse_input(&contents).unwrap();
 
     part1(&rules, &input_strs);
+    part2(&mut rules, &input_strs);
 }
 
 fn part1(rules: &RuleList, input_strs: &Vec<&str>) {
     let match_count = input_strs.iter().filter(|s| rules.matches(&0, **s)).count();
     println!("The number of input strings matching rule 0 is {}", match_count);
+}
+
+fn part2(rules: &mut RuleList, input_strs: &Vec<&str>) {
+    rules.replace_rule(8, "42 | 42 8");
+    rules.replace_rule(11, "42 31 | 42 11 31");
+
+    let matches: Vec<&str> = input_strs.iter().filter(|s| rules.matches(&0, **s)).cloned().collect();
+    println!("The number of input strings matching rule 0 after modifications is {}", matches.len());
 }
--- src/bin/day19/parse.rs
+++ src/bin/day19/parse.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 
+use nom::{Finish, IResult};
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, digit1, newline, satisfy, alpha1};
-use nom::combinator::{map, map_res, all_consuming};
-use nom::{IResult, Finish};
+use nom::character::complete::{alpha1, char, digit1, newline, satisfy};
+use nom::combinator::{all_consuming, map, map_res};
+use nom::error::Error;
 use nom::multi::separated_list1;
 use nom::sequence::{preceded, separated_pair, terminated};
 
@@ -17,15 +18,19 @@ impl RuleList {
         match self.rules.get(rule_num) {
             None => false,
             Some(r) => {
-                match r.recognize(s, &self.rules) {
-                    Some(s) if s.is_empty() => true,
-                    _ => false,
-                }
+                let matches = r.recognize(s, rule_num, &self.rules);
+                matches.iter().any(|s| s.is_empty())
             }
         }
     }
+
+    pub fn replace_rule(&mut self, rule_num: u32, s: &str) {
+        let (_, r) = parse_rule(s).unwrap();
+        self.rules.insert(rule_num, r);
+    }
 }
 
+#[derive(Debug)]
 enum Rule {
     Char(char),
     Subrules(Vec<u32>),
@@ -33,47 +38,43 @@ enum Rule {
 }
 
 impl Rule {
-    fn recognize<'a>(&self, s: &'a str, rules: &HashMap<u32, Rule>) -> Option<&'a str> {
+    fn recognize<'a>(&self, s: &'a str, id: &u32, rules: &HashMap<u32, Rule>) -> Vec<&'a str> {
         use Rule::*;
-
-        if s.is_empty() {
-            return None;
-        }
 
         match self {
             Char(c) => {
-                if s.starts_with(*c) {
-                    Some(&s[1..])
-                } else {
-                    None
+                match char::<&str, Error<&str>>(*c)(s) {
+                    Ok((s, _)) => vec![s],
+                    Err(_) => vec![],
                 }
             }
             Subrules(rule_ids) => {
-                recognize_seq(s, rule_ids, rules)
+                recognize_seq(&[s], id, rule_ids, rules)
             }
             Subrules2(rule_ids1, rule_ids2) => {
-                recognize_seq(s, rule_ids1, rules).or_else(|| recognize_seq(s, rule_ids2, rules))
+                let mut matches = recognize_seq(&[s], id, rule_ids1, rules);
+                matches.append(&mut recognize_seq(&[s], id, rule_ids2, rules));
+                matches
             }
         }
     }
 }
 
-fn recognize_seq<'a>(s: &'a str, rule_ids: &Vec<u32>, rules: &HashMap<u32, Rule>) -> Option<&'a str> {
-    let mut s = s;
-
-    for r in rule_ids {
-        let rule = rules.get(r)?;
-        match rule.recognize(s, rules) {
-            Some(s1) => {
-                s = s1;
-            }
-            None => {
-                return None;
-            }
-        };
+fn recognize_seq<'a>(strs: &[&'a str], id: &u32, rule_ids: &[u32], rules: &HashMap<u32, Rule>) -> Vec<&'a str> {
+    if strs.is_empty() || rule_ids.is_empty() {
+        return strs.to_vec();
     }
 
-    Some(s)
+    let mut res = Vec::new();
+    let r = &rule_ids[0];
+    let rule = rules.get(r).unwrap();
+
+    for s in strs {
+        let matches = rule.recognize(*s, r, rules);
+        res.append(&mut recognize_seq(&matches[..], id, &rule_ids[1..], rules));
+    }
+
+    res
 }
 
 pub fn parse_input(s: &str) -> Result<(RuleList, Vec<&str>), nom::error::Error<&str>> {
@@ -91,7 +92,7 @@ fn parse_rule_list(s: &str) -> IResult<&str, RuleList> {
                 rules_by_num.insert(num, rule);
             }
             RuleList { rules: rules_by_num }
-        }
+        },
     )(s)
 }
 
