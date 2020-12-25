29a92c28d6510b9f67765788391534344e9799d1
Matt Moriarity <matt@mattmoriarity.com>
2020-12-24 21:47:36 -0700

    Solve day 23 part 2

--- src/bin/day23/cups.rs
+++ src/bin/day23/cups.rs
@@ -1,182 +1,66 @@
-use std::num::ParseIntError;
-use itertools::Itertools;
-
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub struct Ring {
-    cups: Vec<u8>,
+    next: Vec<usize>,
     cursor: usize,
 }
 
-impl From<&[u8]> for Ring {
-    fn from(s: &[u8]) -> Self {
-        Ring { cups: s.into(), cursor: 0 }
+impl From<&[usize]> for Ring {
+    fn from(s: &[usize]) -> Self {
+        let mut next = Vec::new();
+        next.resize(s.len() + 1, 0);
+
+        next[s[s.len()-1]] = s[0];
+        for i in 1..s.len() {
+            next[s[i-1]] = s[i];
+        }
+
+        Ring { next, cursor: s[0] }
     }
 }
 
 impl Ring {
-    pub fn from(s: &str) -> Result<Self, ParseIntError> {
-        let elems = s.chars().map(|c| c.to_string().parse::<u8>()).collect::<Result<Vec<u8>, ParseIntError>>()?;
-        Ok((&elems[..]).into())
-    }
-
     pub fn perform_move(&mut self) {
-        let picked_up = self.remove(3);
-        let dest = self.destination_index();
-        self.insert(dest, picked_up);
-        self.advance();
-    }
-
-    /// Removes the next `n` cups after the current cursor position, and returns them in a vector.
-    fn remove(&mut self, n: usize) -> Vec<u8> {
-        let start = self.cursor + 1;
-        let range = start..((start + n).min(self.cups.len()));
-        let num_removed = range.len();
-        let mut removed: Vec<u8> = self.cups.drain(range).collect();
-        if num_removed < n {
-            let range = 0..(n - num_removed);
-            let num_removed = range.len();
-            removed.splice(removed.len()..removed.len(), self.cups.drain(range));
-            self.cursor -= num_removed;
+        let mut removed_elems = Vec::new();
+        for _ in 0..3 {
+            let elem = self.next[self.cursor];
+            self.next[self.cursor] = self.next[elem];
+            removed_elems.push(elem);
         }
-        removed
-    }
 
-    /// Inserts the given `cups` after the cup at the given index.
-    fn insert<I: IntoIterator<Item=u8>>(&mut self, i: usize, cups: I) {
-        let range = (i + 1)..(i + 1);
-        let prev_len = self.cups.len();
-        self.cups.splice(range, cups);
-        if i < self.cursor {
-            self.cursor += self.cups.len() - prev_len;
-        }
-    }
-
-    /// Moves the cursor forward by one cup.
-    fn advance(&mut self) {
-        self.cursor = (self.cursor + 1) % self.cups.len();
-    }
-
-    fn destination_index(&self) -> usize {
-        let mut target_value = self.cups[self.cursor] - 1;
+        let mut target_value = self.cursor - 1;
         if target_value == 0 {
-            target_value = *self.cups.iter().max().unwrap();
+            target_value = self.next.len() - 1;
         }
 
-        loop {
-            match self.cups.iter().find_position(|c| { **c == target_value }) {
-                Some((i, _)) => { return i; }
-                None => {
-                    target_value -= 1;
-                    if target_value == 0 {
-                        target_value = *self.cups.iter().max().unwrap();
-                    }
-                }
+        while removed_elems.contains(&target_value) {
+            target_value -= 1;
+            if target_value == 0 {
+                target_value = self.next.len() - 1;
             }
         }
+
+        let previous_end = self.next[target_value];
+        self.next[target_value] = removed_elems[0];
+        self.next[removed_elems[2]] = previous_end;
+
+        self.cursor = self.next[self.cursor];
     }
 
     pub fn cup_ordering(&self) -> String {
         let mut result = String::new();
-        let (one_pos, _) = self.cups.iter().find_position(|c| **c == 1).unwrap();
 
-        for cup in &self.cups[(one_pos+1)..] {
-            result += &cup.to_string();
-        }
-
-        for cup in &self.cups[..one_pos] {
-            result += &cup.to_string();
+        let mut cur_val = self.next[1];
+        while cur_val != 1 {
+            result += &cur_val.to_string();
+            cur_val = self.next[cur_val];
         }
 
         result
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use std::num::ParseIntError;
-
-    use crate::cups::Ring;
-
-    #[test]
-    fn from_string() -> Result<(), ParseIntError> {
-        assert_eq!(Ring {
-            cups: vec![3, 2, 4, 1, 5],
-            cursor: 0,
-        }, Ring::from("32415")?);
-        assert_eq!(Ring {
-            cups: vec![4, 6, 3, 5, 2, 8, 1, 7, 9],
-            cursor: 0,
-        }, Ring::from("463528179")?);
-
-        Ok(())
-    }
-
-    #[test]
-    fn advance() -> Result<(), ParseIntError> {
-        let mut ring = Ring::from("32415")?;
-        assert_eq!(0, ring.cursor);
-        ring.advance();
-        assert_eq!(1, ring.cursor);
-
-        // test wrapping at the end
-        ring.cursor = 4;
-        ring.advance();
-        assert_eq!(0, ring.cursor);
-
-        Ok(())
-    }
-
-    #[test]
-    fn remove() -> Result<(), ParseIntError> {
-        let mut ring = Ring::from("32415")?;
-        assert_eq!(vec![2, 4, 1], ring.remove(3));
-        assert_eq!(vec![3, 5], ring.cups);
-        assert_eq!(0, ring.cursor);
-
-        // test wrapping behavior
-        ring = Ring::from("32415")?;
-        ring.cursor = 3;
-        assert_eq!(vec![5, 3, 2], ring.remove(3));
-        assert_eq!(vec![4, 1], ring.cups);
-        assert_eq!(1, ring.cursor);
-
-        ring = Ring::from("32415")?;
-        ring.cursor = 4;
-        assert_eq!(vec![3, 2, 4], ring.remove(3));
-        assert_eq!(vec![1, 5], ring.cups);
-        assert_eq!(1, ring.cursor);
-
-        Ok(())
-    }
-
-    #[test]
-    fn insert() -> Result<(), ParseIntError> {
-        let mut ring = Ring::from("32415")?;
-        ring.advance();
-        let removed_cups = ring.remove(3);
-        ring.insert(0, removed_cups);
-        assert_eq!(vec![3, 4, 1, 5, 2], ring.cups);
-        assert_eq!(4, ring.cursor);
-
-        ring = Ring::from("32415")?;
-        let removed_cups = ring.remove(3);
-        ring.insert(1, removed_cups);
-        assert_eq!(vec![3, 5, 2, 4, 1], ring.cups);
-        assert_eq!(0, ring.cursor);
-
-        Ok(())
-    }
-
-    #[test]
-    fn destination_index() -> Result<(), ParseIntError> {
-        let mut ring = Ring::from("32415")?;
-        ring.remove(3);
-        assert_eq!(1, ring.destination_index());
-
-        ring = Ring::from("389154267")?;
-        ring.remove(3);
-        assert_eq!(3, ring.destination_index());
-
-        Ok(())
+    pub fn cups_after_one(&self) -> (usize, usize) {
+        let first = self.next[1];
+        let second = self.next[first];
+        (first, second)
     }
 }
--- src/bin/day23/main.rs
+++ src/bin/day23/main.rs
@@ -1,19 +1,34 @@
+use itertools::Itertools;
+
 use crate::cups::Ring;
 
 mod cups;
 
-const INPUT: &str = "463528179";
-
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
-    let mut ring = Ring::from(INPUT).unwrap();
+    let input: [usize; 9] = [4, 6, 3, 5, 2, 8, 1, 7, 9];
+    let mut ring: Ring = (&input[..]).into();
     for _ in 0..100 {
         ring.perform_move();
     }
 
     let ordering = ring.cup_ordering();
     println!("The ordering of the cups is now {}", ordering);
+}
+
+fn part2() {
+    let initial_nums: [usize; 9] = [4, 6, 3, 5, 2, 8, 1, 7, 9];
+    let nums = (&initial_nums[..]).iter().cloned().chain(10..=1000000).collect_vec();
+
+    let mut ring: Ring = (&nums[..]).into();
+    for _ in 0..10000000 {
+        ring.perform_move();
+    }
+
+    let (first, second) = ring.cups_after_one();
+    println!("The cups with the stars under them are {} * {} = {}", first, second, first * second);
 }
